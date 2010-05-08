c1565fd6efd733fbc032405091609856eb329918
David E. Wheeler <david@justatheory.com>
2010-05-07 20:09:45 -0700

    Add `Build.PL` and basic files.

---
+++ lib/App/FeedScene.pm
@@ -0,0 +1,21 @@
+package App::FeedScene 0.01;
+
+use strict;
+use warnings;
+use 5.12.0;
+use utf8;
+
+=head1 Name
+
+App::FeedScene - FeedScene feed processor
+
+=head1 Author
+
+David E. Wheeler <david@kineticode.com>
+
+=head1 Copyright
+
+Copyright (c) 2010 David E. Wheeler. All rights reserved.
+
+=cut
+
